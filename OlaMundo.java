528c6f4eeea755cca26a73031d69d15802b0f847
Bruno Francisco <brunoffco@gmail.com>
2023-09-11 20:41:13 -0300

    Update OlaMundo.java

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira um numero");
+        int numero = scanner.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("O numero " + numero +" eh par");
+        } else {
+            System.out.println("O numero " + numero +" eh impar");
+        }
+        scanner.close();
+        }
+        
     }
-}
