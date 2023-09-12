5704a046d4486388b354c33b1116994a9b8d192c
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-09-12 15:43:15 -0300

    Adicionado arquivos gitignore, corrigido o nome das classes e indentação

--- aula_09_11/TesteValor.java
+++ aula_09_11/TesteValor.java
@@ -1,15 +1,14 @@
 import javax.swing.JOptionPane;
 
 public class TesteValor {
-    public static void main(String[] args) {
-        int n = Integer.parseInt(
-                JOptionPane.showInputDialog("Digite um inteiro"));
+	public static void main(String[] args) {
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Digite um inteiro"));
 
-        if (n >= 100) {
-            JOptionPane.showMessageDialog(null, n + " é maior ou igual a 100");
-        } else {
-            JOptionPane.showMessageDialog(null, n + " é menor que 100");
-        }
+		if (n >= 100) {
+			JOptionPane.showMessageDialog(null, n + " é maior ou igual a 100");
+		} else {
+			JOptionPane.showMessageDialog(null, n + " é menor que 100");
+		}
 
-    }
+	}
 }
