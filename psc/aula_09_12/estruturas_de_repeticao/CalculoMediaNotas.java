0a0a40e1a929445d5d907da8b49542ebc477179e
Bruno Francisco <brunoffco@gmail.com>
2023-09-12 20:29:35 -0300

    Create CalculoMediaNotas.java

---
+++ psc/aula_09_12/estruturas_de_repeticao/CalculoMediaNotas.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CalculoMediaNotas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double nota1, nota2;
+        int contador = 1;
+        
+        while (contador <= 10) {
+            System.out.print("Digite a nota 1: ");
+            nota1 = scanner.nextDouble();
+            
+            System.out.print("Digite a nota 2: ");
+            nota2 = scanner.nextDouble();
+            
+            double media = (nota1 + nota2) / 2;
+            System.out.println("Média: " + media);
+            
+            contador++;
+        }
+        
+        scanner.close();
+    }
+}
