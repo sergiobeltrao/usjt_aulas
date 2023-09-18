1d5e2d7f06521b04e1a67462bf7b48cf6e9cc884
Bruno Francisco <brunoffco@gmail.com>
2023-09-18 20:05:32 -0300

    Create Conta1a100.java

---
+++ psc/aula_09_18/Conta1a100.java
@@ -0,0 +1,17 @@
+package psc.aula_09_18;
+
+public class Conta1a100 {
+    public static void main(String[] args) {
+        int contador=1;
+       System.out.println("Numeros de 1 a 100"); 
+        while (contador <= 100) {
+            System.out.printf(contador + " ");
+            contador++;
+        }
+        System.out.println("\n\nDe Novo");
+        for (contador=1; contador<=100; contador++)
+        System.out.print(contador + " ");
+            {
+        }
+    }
+}
