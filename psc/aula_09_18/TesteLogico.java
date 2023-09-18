177380aa0ee2ee3643cda3f319d3994fd5b4cede
Bruno Francisco <brunoffco@gmail.com>
2023-09-18 20:05:29 -0300

    Create TesteLogico.java

---
+++ psc/aula_09_18/TesteLogico.java
@@ -0,0 +1,11 @@
+//Operadores Relacionais: >, >; <, <; ==; |;
+
+//Operadores Lógicos: && => e; || => ou; ! => negação
+
+// ¹ Inicia a condição
+    // while (² teste condição) {
+    // ³ atulaizar condição/contador;}
+    
+            // Ou 
+
+// {for (¹ inicie a condição/contador; teste condição; atualização)}
