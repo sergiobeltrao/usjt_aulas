9cb248d28f84eb6db98cab52527671be08832823
Bruno Francisco <brunoffco@gmail.com>
2023-09-19 19:30:51 -0300

    Update SomaDoisNumeros.java

--- psc/aula_09_05/SomaDoisNumeros.java
+++ psc/aula_09_05/SomaDoisNumeros.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
 
-public class SomaDoisNumeros{
+public class SomaDoisNumeros {
     public static void main(String [] args){
         Scanner leitor = new Scanner (System.in);
         double primeiroOperando;
