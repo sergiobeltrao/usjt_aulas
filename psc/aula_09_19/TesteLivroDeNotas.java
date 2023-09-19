112ec184dad96c366e06bc825489ce0ab7f365b6
Bruno Francisco <brunoffco@gmail.com>
2023-09-19 20:31:20 -0300

    Reseting TesteLivroDeNotas.java

--- psc/aula_09_19/TesteLivroDeNotas.java
+++ psc/aula_09_19/TesteLivroDeNotas.java
@@ -1,9 +1,11 @@
 package psc.aula_09_19;
 
+import static javax.swing.JOptionPane.showInputDialog;;
 public class TesteLivroDeNotas {
-    public void exibirMensagem() {
-        System.out.println("Bem-vindo ao livro de notas!");
+    public static void main(String[] args) {
+        String nomeDisciplina = showInputDialog("Digite o nome da disciplina");
+        LivroDeNotas livro = new LivroDeNotas();
+        livro.exibirMensagem(nomeDisciplina);
+        System.out.println("Até logo");
     }
 }
-
-//Arquivo não pode ser executado por não conter um metodo main
