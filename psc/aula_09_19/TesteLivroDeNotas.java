3879e7c33b70928ce4c29698c023a8f8dfa1b17e
Bruno Francisco <brunoffco@gmail.com>
2023-09-19 20:00:09 -0300

    Create TesteLivroDeNotas.java

---
+++ psc/aula_09_19/TesteLivroDeNotas.java
@@ -0,0 +1,9 @@
+package psc.aula_09_19;
+
+public class TesteLivroDeNotas {
+    public void exibirMensagem() {
+        System.out.println("Bem-vindo ao livro de notas!");
+    }
+}
+
+//Arquivo não pode ser executado por não conter um metodo main
