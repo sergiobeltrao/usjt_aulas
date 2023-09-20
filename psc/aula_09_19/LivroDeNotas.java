b74b3cc8b4d134194c834a30ec389025945bb6f6
Bruno Francisco <brunoffco@gmail.com>
2023-09-19 21:12:10 -0300

    Update LivroDeNotas.java

--- psc/aula_09_19/LivroDeNotas.java
+++ psc/aula_09_19/LivroDeNotas.java
@@ -3,7 +3,7 @@ package psc.aula_09_19;
 
 public class LivroDeNotas {
     public void exibirMensagem(String nomeDisciplina) {
-        System.out.println("Bem-vindo ao livro de notas de" + nomeDisciplina);
+        System.out.println("Bem-vindo ao livro de notas de " + nomeDisciplina );
     }
 
 }
