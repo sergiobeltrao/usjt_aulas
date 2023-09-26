41496642a31073ae20be74916c58c4997328a1bd
Bruno Francisco <brunoffco@gmail.com>
2023-09-25 21:31:40 -0300

    Update Jogo.java

--- psc/aula_09_25/Jogo.java
+++ psc/aula_09_25/Jogo.java
@@ -1,13 +1,24 @@
 public class Jogo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Personagem cacador = new Personagem();
         cacador.nome = "Bruno";
-        cacador.cacar();
-        cacador.comer();
+//         cacador.cacar();
+//         cacador.comer();
 
-        Personagem lenhador = new Personagem();
-        lenhador.nome = "Diego";
-        lenhador.cacar();
-        lenhador.dormir();
+//         Personagem lenhador = new Personagem();
+//         lenhador.nome = "Diego";
+//         lenhador.cacar();
+//         lenhador.dormir();
+        while (true) {
+            cacador.cacar();
+            cacador.comer();
+            cacador.dormir();
+            cacador.cacar();
+            cacador.comer();
+            cacador.dormir();
+            System.out.println("==================");
+            Thread.sleep(2000);
+
+        }
     }
 }
