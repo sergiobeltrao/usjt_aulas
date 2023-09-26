654424a543ad06bcec9e221075daa440efdd89a4
Bruno Francisco <brunoffco@gmail.com>
2023-09-26 20:03:40 -0300

    New Jogo2.java

--- psc/aula_09_25/Jogo2.java
+++ psc/aula_09_25/Jogo2.java
@@ -1,23 +1,16 @@
-import java.util.Random;
-
 public class Jogo2 {
     public static void main(String[] args) throws InterruptedException {
         Personagem cacador = new Personagem();
         cacador.nome = "Bruno";
-        Random random = new Random();
         while (true) {
-            int sorteio = random.nextInt(3);
-            if (sorteio == 0) {
-                cacador.cacar();
-            }
-            else if (sorteio == 1) {
-                cacador.comer();  
-            }
-            else {
-                cacador.dormir();
-            }
-            System.out.println("================================");
-            Thread.sleep(1000);
+            cacador.cacar();
+            cacador.comer();
+            cacador.dormir();
+            cacador.cacar();
+            cacador.comer();
+            cacador.dormir();
+            System.out.println("==================");
+            Thread.sleep(2000);
         }
     }
 }
