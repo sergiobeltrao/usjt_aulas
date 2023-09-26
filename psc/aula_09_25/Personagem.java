c76216e50dba1cfe1b7eb415efaa9d4f5dca81f1
Bruno Francisco <brunoffco@gmail.com>
2023-09-25 21:31:42 -0300

    Update Personagem.java

--- psc/aula_09_25/Personagem.java
+++ psc/aula_09_25/Personagem.java
@@ -3,12 +3,36 @@ public class Personagem {
     int energia, fome, sono;
 
     void cacar () {
-        System.out.println(nome +" esta cacando...");
+        if (energia >= 2) {
+            System.out.println(nome +" esta cacando");
+            energia = energia - 2;
+            if (fome < 10) {
+             fome = fome++;
+            }
+            sono = Math.min(sono + 1, 10);
+        } 
+        else {
+            System.out.println(nome +" esta sem energia para cacar");
+        }
     }
     void comer () {
-        System.out.println(nome +" esta comendo...");
+        if (fome>= 1) {
+            System.out.println(nome +" esta comendo...");
+            fome--;
+            energia = Math.min(10, energia++);
+        }
+        else {
+            System.out.println(nome +" esta sem fome...");
+        }
     }
     void dormir () {
-        System.out.println(nome +" esta dormindo");
+       if(sono>=1) {
+            System.out.println(nome +" esta dormindo...");
+            sono--;
+            energia = energia < 10? energia++: 10;
+        }
+       else {
+            System.out.println(nome + "esta sem sono...");
+        }
     }
 }
