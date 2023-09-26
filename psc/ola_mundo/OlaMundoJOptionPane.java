cb9eea30ad59519cc7e638aefd3e5773185766b6
Bruno Francisco <brunoffco@gmail.com>
2023-09-26 20:10:01 -0300

    Create OlaMundoJOptionPane.java

---
+++ psc/ola_mundo/OlaMundoJOptionPane.java
@@ -0,0 +1,9 @@
+package psc.ola_mundo;
+
+import javax.swing.JOptionPane;
+
+public class OlaMundoJOptionPane {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Olá, Mundo!");
+    }
+}
