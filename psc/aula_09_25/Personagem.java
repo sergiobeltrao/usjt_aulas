e22302ee8bef59f6ce5fee59cae92d5e1135bc48
Bruno Francisco <brunoffco@gmail.com>
2023-09-26 21:45:10 -0300

    Update Personagem.java

--- psc/aula_09_25/Personagem.java
+++ psc/aula_09_25/Personagem.java
@@ -1,8 +1,34 @@
+import java.security.PublicKey;
+
 public class Personagem {
-    String nome;
-    int energia = 10;
-    int fome = 0;
-    int sono = 0;
+    private String nome;
+    private int energia;
+    private int fome;
+    private int sono;
+    private int vida;
+
+    public Personagem (String nome) {
+        this.nome = nome;
+    }
+
+    public Personagem (String nome, int energia, int fome, int sono, int vida) {
+        this(nome);
+        if (energia >= 0 && energia <= 10) {
+            this.energia = energia;
+        }
+         if (fome >= 0 && fome <= 10) {
+            this.fome = fome;
+        }
+            else {
+                this.energia = 10;
+            }
+        if (sono >= 0 && sono <= 10) {
+            this.sono = sono;
+        }
+         if (vida >= 0 && vida <= 10) {
+            this.vida = vida;
+        }
+    }
 
     void cacar() {
         if (energia >= 2) {
@@ -15,7 +41,7 @@ public class Personagem {
             
             sono = Math.min(sono + 1, 10);
         } else {
-            System.out.println("sem energia para cacar...");
+            System.out.println(nome + " sem energia para cacar...");
         }
     }
 
@@ -37,5 +63,16 @@ public class Personagem {
         } else {
             System.out.println(nome + " sem sono...");
         }
+    }
+    void vida() {
+        if (vida <= 0) {
+            System.out.println(nome + " morreu...");
+            vida--;
+        } else {
+            System.out.println(nome + " esta vivo...");
+        }
+    }
+    void exibirAtributos() {
+        System.out.println(nome + ":\nenergia: " + energia + "\nfome: " + fome + "\nsono: " + sono);
     }
 }
