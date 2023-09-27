f26f426cb81e934605906028cdbf47eebf13d2df
Bruno Francisco <brunoffco@gmail.com>
2023-09-26 21:45:02 -0300

    Update Jogo1.java

--- psc/aula_09_25/Jogo1.java
+++ psc/aula_09_25/Jogo1.java
@@ -1,12 +1,11 @@
 public class Jogo1 {
     public static void main(String[] args) {
-        Personagem cacador = new Personagem();
-        cacador.nome = "Bruno";
+        Personagem cacador = new Personagem("Bruno", 10, 0, 0, 0);
         cacador.cacar();
         cacador.comer();
+        cacador.vida();
 
-        Personagem lenhador = new Personagem();
-        lenhador.nome = "Diego";
+        Personagem lenhador = new Personagem("Diego");
         lenhador.cacar();
         lenhador.dormir();
 
