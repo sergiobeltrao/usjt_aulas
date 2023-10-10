4e12108a033a6ac2fe27b41f94f0008f4561d8dc
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-10-10 20:09:49 -0300

    Atualizado

--- psc/Aula 10-10/cadastroPessoas/src/main/java/ConnectionFactory.java
+++ psc/Aula 10-10/cadastroPessoas/src/main/java/ConnectionFactory.java
@@ -1,7 +1,27 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionFactory {
+
     private static String usuario = "root";
     private static String senha = "12345";
     private static String host = "localhost";
-    
+    private static String porta = "3006";
+    private static String db = "db_pessoas";
+
+    public static Connection obtemConexao() {
+        try {
+            Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://" + host + ":" + porta + "/" + db + "?user=" + usuario + "&password=" + senha
+                    + "&useTimezone=true&serverTimezone=America/Sao_Paulo");
+            return c;
+
+        } catch (Exception e) {
+            // Exibe o erro e continua
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
