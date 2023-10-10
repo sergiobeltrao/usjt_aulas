7716a2c4b1e3126aecc7387c0f7d4dc68661a231
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-10-10 19:05:11 -0300

    Algumas mudancas

--- psc/aula_09_10/tela_login/src/main/java/ConnectionFactory.java
+++ psc/aula_09_10/tela_login/src/main/java/ConnectionFactory.java
@@ -5,15 +5,16 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
 
     private static String usuario = "root";
-    private static String senha = "1234";
+    private static String senha = "anima123";
     private static String host = "localhost";
     private static String porta = "3306";
-    private static String bd = "db_pessoas";
+    private static String bd = "tb_usuario";
 
     public static Connection obtemConexao() {
         try {
-            return DriverManager.getConnection(
-                    "jdbc:mysql://" + host + ":" + porta + "/" + bd + "?user="+usuario+"&password="+senha+"&useTimezone=true&serverTimezone=America/Sao_Paulo");
+            return DriverManager
+                    .getConnection("jdbc:mysql://" + host + ":" + porta + "/" + bd + "?user=" + usuario + "&password=" + senha + "&sueTimezone=true&serverTimezone=America/Sao+Paulo");
+            // teste
         } catch (Exception e) {
             e.printStackTrace();
             return null;
