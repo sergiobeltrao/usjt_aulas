fef556f5f52c24b39ee37ef0cece5a3940959aa8
Bruno Francisco <brunoffco@gmail.com>
2023-10-19 19:18:56 -0300

    Update Lucro.java

--- psc/A1/Lucro.java
+++ psc/A1/Lucro.java
@@ -1,55 +1,47 @@
 package psc.A1;
 
+
+import javax.swing.JOptionPane;
 import java.util.Locale;
-import java.util.Scanner;
 
 public class Lucro {
-	private int numeroClientes;
-	private double valorTotalGasto;
+    private int numeroClientes;
+    private double valorTotalGasto;
 
-	public void calcularMediaGastoPorCliente() {
-		Scanner leitor = new Scanner(System.in);
+    public void calcularMediaGastoPorCliente() {
+        String input = JOptionPane.showInputDialog("Digite o número de clientes:");
+        numeroClientes = Integer.parseInt(input);
 
-		System.out.print("Digite o número de clientes: ");
-		numeroClientes = leitor.nextInt();
-		System.out.println();
+        if (numeroClientes <= 0) {
+            JOptionPane.showMessageDialog(null, "Desculpe, mas o número mínimo de clientes é 1.");
+        } else {
+            double mediaDoCliente = 0;
+            for (int i = 1; i <= numeroClientes; i++) {
+                double valorBebida = Double.parseDouble(JOptionPane.showInputDialog("Cliente " + i + ". Digite o valor gasto em bebidas:"));
 
-		if (numeroClientes == 0 || numeroClientes < 0) {
-			System.err.println("Desculpe, mas o número mínimo de clientes é 1.");
-		} else {
-			double mediaDoCliente = 0;
-			for (int i = 1; i <= numeroClientes; i++) {
-				System.out.printf("Cliente %d. Digite o valor gasto em bebidas: R$ ", i);
-				double valorBebida = leitor.nextDouble();
+                double valorComida = Double.parseDouble(JOptionPane.showInputDialog("Cliente " + i + ". Digite o valor gasto em comidas:"));
 
-				System.out.printf("Cliente %d. Digite o valor gasto em comidas: R$ ", i);
-				double valorComida = leitor.nextDouble();
+                double valorGasto = valorBebida + valorComida;
+                valorTotalGasto += valorGasto;
+                mediaDoCliente = valorGasto / 2;
+                JOptionPane.showMessageDialog(null, "Média de gasto pelo cliente: R$ " + String.format("%.2f", mediaDoCliente));
+            }
+        }
+    }
 
-				double valorGasto = valorBebida + valorComida;
-				valorTotalGasto = valorTotalGasto + valorGasto;
-				mediaDoCliente = valorGasto / 2;
-				System.out.printf("Média de gasta pelo cliente: R$ %.2f%n", mediaDoCliente);
-				System.out.println();
-			}
+    public void calcularMediaGeral() {
+        if (numeroClientes > 0) {
+            double mediaGastoPorCliente = valorTotalGasto / numeroClientes;
+            JOptionPane.showMessageDialog(null, "Média geral de gastos por cliente: R$ " + String.format("%.2f", mediaGastoPorCliente));
+        }
+    }
 
-		}
-		leitor.close();
-	}
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+        JOptionPane.showMessageDialog(null, "Atenção! Forneça os dados separando os valores decimais por uma vírgula.");
 
-	public void calcularMediaGeral() {
-		if (numeroClientes > 0) {
-			double mediaGastoPorCliente = valorTotalGasto / numeroClientes;
-			System.out.printf("Média geral de gastos por cliente: R$ %.2f%n", mediaGastoPorCliente);
-		}
-	}
-
-	public static void main(String[] args) {
-		Locale.setDefault(new Locale("pt", "BR"));
-
-		System.out.println("Atenção! Forneça os dados separando os valores decimais por uma vírgula.");
-
-		Lucro sistema = new Lucro();
-		sistema.calcularMediaGastoPorCliente();
-		sistema.calcularMediaGeral();
-	}
+        Lucro sistema = new Lucro();
+        sistema.calcularMediaGastoPorCliente();
+        sistema.calcularMediaGeral();
+    }
 }
