547675b1bd7945baa26adceeefcaa33fcae9cea5
Bruno Francisco <brunoffco@gmail.com>
2023-10-20 21:28:01 -0300

    Update AluguelFestas.sql

--- MS/AluguelFestas.sql
+++ MS/AluguelFestas.sql
@@ -2,22 +2,16 @@ CREATE DATABASE AluguelFestas;
 
 USE AluguelFestas;
 
-CREATE TABLE Item_Tema (
-    ID INT PRIMARY KEY,
-    Descricao TEXT NOT NULL
-);
-
 CREATE TABLE Tema (
     ID INT PRIMARY KEY,
     Nome VARCHAR(255) NOT NULL,
     ValorAluguel NUMERIC(8,2) NOT NULL
 );
-
-CREATE TABLE Festa (
-	ID INT PRIMARY KEY,
-    ValorCobrado NUMERIC(8,2) NOT NULL,
-    DataInicio DATE,
-    DataFim DATE
+CREATE TABLE Item_Tema (
+    ID INT PRIMARY KEY,
+    Descricao TEXT NOT NULL,
+	Tema_ID INT,
+	FOREIGN KEY (Tema_ID) REFERENCES Tema(ID)
 );
 
 CREATE TABLE Endereco (
@@ -28,8 +22,23 @@ CREATE TABLE Endereco (
     UF CHAR(2) NOT NULL,
     CEP CHAR(8) NOT NULL
 );
+
 CREATE TABLE Cliente (
     CPF INT PRIMARY KEY,
     Nome VARCHAR(255) NOT NULL,
     Telefone VARCHAR(15) NOT NULL
 );
+
+CREATE TABLE Festa (
+	ID INT PRIMARY KEY,
+    ValorCobrado NUMERIC(8,2) NOT NULL,
+    DataInicio DATE,
+    DataFim DATE,
+     Endereco_ID INT,
+    Cliente_CPF INT,
+    Tema_ID INT,
+	FOREIGN KEY (Endereco_ID) REFERENCES Endereco(ID),
+    FOREIGN KEY (Cliente_CPF) REFERENCES Cliente(CPF),
+    FOREIGN KEY (Tema_ID) REFERENCES Tema(ID)
+);
+
