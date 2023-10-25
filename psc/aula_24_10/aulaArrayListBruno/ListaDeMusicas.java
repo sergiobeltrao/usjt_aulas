db772bbc948673d8cab7d2820ed15d856caad123
Bruno Francisco <brunoffco@gmail.com>
2023-10-24 21:39:06 -0300

    Uptade ArrayList

--- psc/aula_24_10/aulaArrayListBruno/ListaDeMusicas.java
+++ psc/aula_24_10/aulaArrayListBruno/ListaDeMusicas.java
@@ -1,5 +1,40 @@
 package psc.aula_24_10.aulaArrayListBruno;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 public class ListaDeMusicas {
-    
+    public static void main(String[] args) {
+        List<Musica> musicas = new ArrayList<>();
+        int opcao;
+        do {
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite\n0 - Sair\n1 - Nova Musica\n2 - Avaliar Musica\n3 - Listar Musicas"));
+            switch (opcao) {
+                case 0:
+                JOptionPane.showMessageDialog(null,"Obrigado, Volte Sempre :)");
+                break;
+                case 1:
+                    String nome = JOptionPane.showInputDialog(" Digite o nome da musica:");
+                    Musica musica = new Musica(nome);
+                    musicas.add(musica);
+                  /*   if (m.cadastrar()) {
+                        JOptionPane.showMessageDialog(null, " Cadastro realizado com sucesso :) ");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, " Cadastro falhou :( ");
+                    }*/
+                break;
+                case 2:
+
+                break;
+                case 3:
+
+                break;
+                default:
+                JOptionPane.showMessageDialog(null,"Opcao Invalida :(");
+            }
+        }while (opcao !=0);
+    }
 }
---
+++ psc/aula_24_10/aulaArrayListBruno/Musica.java
@@ -0,0 +1,23 @@
+package psc.aula_24_10.aulaArrayListBruno;
+
+public class Musica {
+    private String nome;
+    private int avaliacao;
+    
+    public Musica (String nome) {
+        this.nome = nome;
+    }
+
+    public  String getNome () {
+        return this.nome;
+    }
+
+    public int getAvaliacao () {
+        return this.avaliacao;
+    }
+
+    public void setAvaliacao (int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+}
