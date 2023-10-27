19b4f1bc08da3cba6f962609cea7f5837a901003
Bruno Francisco <brunoffco@gmail.com>
2023-10-27 17:10:03 -0300

    refactor(PreSta): refeita melhorar utilidade

--- src/main/java/com/mycompany/bibliotech/connection/PreparedStatement.java
+++ src/main/java/com/mycompany/bibliotech/connection/PreparedStatement.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.bibliotech.connection;
 
-/**
- *
- * @author bruno
- */
-public class PreparedStatement {
-    
+import java.sql.ResultSet;
+
+class PreparedStatement {
+
+    ResultSet executeQuery() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
 }
+
