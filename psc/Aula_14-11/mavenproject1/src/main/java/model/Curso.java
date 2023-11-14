3f602cdd2c4b770bcb0c42bdc8b7cb11b549afff
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-11-14 19:14:01 -0300

    Aula 14-11

---
+++ psc/Aula_14-11/mavenproject1/src/main/java/model/Curso.java
@@ -0,0 +1,54 @@
+package model;
+
+
+public class Curso {
+
+    private int id;
+    private String nome;
+    private String tipo;
+
+    public Curso(int id) {
+        this.id = id;
+    }
+
+    public Curso(int id, String nome, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public Curso(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+
+}
